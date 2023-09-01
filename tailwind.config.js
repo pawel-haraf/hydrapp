ba682c0edb60909dc46553588a85bfcfe721822b
Pawel Haraf <haraf.p@gmail.com>
2023-09-02 00:55:06 +0200

    installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				darkblue: '#0F172A',
+				whiteglass: '#ECEFF1',
+				water: '#60A5FA',
+			},
+		},
+	},
+	plugins: [],
+}
