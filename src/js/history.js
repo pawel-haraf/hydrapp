d3da5e609c93557a699316209ee05e466b9e61c7
Pawel Haraf <haraf.p@gmail.com>
2023-09-02 23:51:07 +0200

    Added border in table

--- src/js/history.js
+++ src/js/history.js
@@ -5,6 +5,7 @@ for (let i = 0; i < localStorage.length; i++) {
 	let key = localStorage.key(i)
 	let value = localStorage.getItem(localStorage.key(i))
 	let el = document.createElement('tr')
+	el.classList = 'border'
 	el.innerHTML = `<td>${i + 1}</td><td>${key}</td><td>${value}</td>`
 	table.appendChild(el)
 }
