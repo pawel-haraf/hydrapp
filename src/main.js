ae1969cf4643c6b9acde72a52b5c0497260351e0
Pawel Haraf <haraf.p@gmail.com>
2023-09-02 12:52:59 +0200

    Created if function & checked day is today

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 const add = document.querySelector('.add--js')
 const remove = document.querySelector('.remove--js')
 const text = document.querySelector('.countdown--js')
+const key = new Date().toISOString().slice(0, 10)
+const day = JSON.parse(localStorage.getItem(key))
 
-console.log(add)
-console.log(remove)
-console.log(text)
-add.addEventListener('click', () => {
-	let value = Number(text.textContent) + 1
+if (key === key && day !== null) {
+	let value = JSON.parse(localStorage.getItem(key))
 	text.innerHTML = value
+} else {
+	localStorage.setItem(key, 0)
+	text.innerHTML = 0
+}
+
+add.addEventListener('click', () => {
+	value = Number(text.textContent) + 1
+	text.innerHTML = value
+	localStorage.setItem(key, value)
 })
+
 remove.addEventListener('click', () => {
 	let value = Number(text.textContent)
 	if (value > 0) {
 		value = value - 1
 		text.innerHTML = value
+		localStorage.setItem(key, value)
 	} else {
 		text.innerHTML = 0
+		localStorage.setItem(key, value)
 	}
 })
