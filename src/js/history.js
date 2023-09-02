84f9b99ac42cb7c044e0d262c1bd565201f82966
Pawel Haraf <haraf.p@gmail.com>
2023-09-02 23:19:48 +0200

    Created funtion which created new row in table

--- src/js/history.js
+++ src/js/history.js
@@ -1,5 +1,10 @@
+const table = document.querySelector('.table--js')
+console.log(table)
+
 for (let i = 0; i < localStorage.length; i++) {
 	let key = localStorage.key(i)
 	let value = localStorage.getItem(localStorage.key(i))
-	console.log(`W dniu ${key} wypiłeś ${value} szklanek`)
+	let el = document.createElement('tr')
+	el.innerHTML = `<td>${i + 1}</td><td>${key}</td><td>${value}</td>`
+	table.appendChild(el)
 }
